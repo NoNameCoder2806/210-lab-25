901eee9a2a4df84e76c5c817e59344e9284d0007
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-10-21 17:14:56 -0700

    Trying to implement read() function

--- main.cpp
+++ main.cpp
@@ -22,16 +22,16 @@ const int DATA_STRUCTURES = 3;
 
 // Function prototypes
 template<typename T>
-long long read(T dataStructure);
+long long read(T dataStructure, dataStructureType type);
 
 template<typename T>
-long long sorting(T dataStructure);
+long long sorting(T dataStructure, dataStructureType type);
 
 template<typename T>
-long long insertion(T dataStructure);
+long long insertion(T dataStructure, dataStructureType type);
 
 template<typename T>
-long long deletion(T dataStructure);
+long long deletion(T dataStructure, dataStructureType type);
 
 // Main function
 int main()
@@ -69,31 +69,65 @@ int main()
 
 // Function implementations
 template<typename T>
-long long read(T dataStructure)
+long long read(T dataStructure, dataStructureType type)
 {
     // Create a file stream
     ifstream fin;
     fin.open(DATA_PATH);
 
-    // Read the data and insert it into the data structure
+    // Check for file stream error
+    if (!fin.is_open())
+    {
+        // Display an error message
+        cerr << "Error: could not open file " << DATA_PATH << endl;
+        
+        // Exit the function
+        return -1;
+    }
+
+    // Create a string variable to store the line data
+    string line = "";
+
+    // Start timing
+    auto start = high_resolution_clock::now();
+
+    // Check for the data structure type
+    if (type == VECTOR || type == LIST)
+    {
+        // Read the data
+        while (getline(fin, line))
+        {
+            // Use push_back() for vectors and lists
+            dataStructure.push_back(line);
+        }
+    }
+    else if (type == SET)
+    {
+        // Read the data
+        while (getline(fin, line))
+        {
+            // Use insert() for sets
+            dataStructure.insert(line);
+        }
+    }
     
     return 0;
 }
 
 template<typename T>
-long long sorting(T dataStructure)
+long long sorting(T dataStructure, dataStructureType type)
 {
     return 0;
 }
 
 template<typename T>
-long long insertion(T dataStructure)
+long long insertion(T dataStructure, dataStructureType type)
 {
     return 0;
 }
 
 template<typename T>
-long long deletion(T dataStructure)
+long long deletion(T dataStructure, dataStructureType type)
 {
     return 0;
 }
